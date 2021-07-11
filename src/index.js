b99a20ae047389715867802ec93a6c0a0c7076a9
K1ndum <kindum98@gmail.com>
2021-07-11 19:04:44 +0300

    Task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        let num = n * (-1);
+        var str = String(num);
+    } else {
+        let num = n;
+        var str = String(num);
+    }
+  return (str.split('').reverse().join(''));
 }
